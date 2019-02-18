6a61029bf510cdb9c7ff1201a0244126ae54c43b
sophielem <lematre.sophiegmail.com>
2019-02-18 10:46:48 +0100

    intersection node and leaf func

--- bin/intersection.py
+++ bin/intersection.py
@@ -6,6 +6,7 @@ DEBUG = True
 import os
 import argparse
 import pickle
+import time
 import common_functions as cf
 import allgenomes as ag
 
@@ -20,7 +21,8 @@ def args_gestion():
     parser.add_argument("-nodes", metavar="<str>",
                         help="List of nodes for the node", required=True)
     parser.add_argument("-rfg", metavar="<str>",
-                        help="The path to the reference genome folder")
+                        help="The path to the reference genome folder",
+                        required=True)
     args = parser.parse_args()
     return args
 
@@ -32,32 +34,67 @@ def setup_application(param):
     leaves = param.leaves.split("&")
     nodes = param.nodes.split("&")
     if leaves == [""]: leaves = []
-    if nodes == [""]: nodes = []
+    if nodes == [""]:
+        nodes = []
+    else:
+        nodes = [param.rfg + "/node/" + node + ".p" for node in nodes]
     return leaves, nodes
 
 
-def fusion_dic(dic_leaves, dic_nodes):
+def fusion_nodes(dic_ref, list_nodes):
     """
-    Fusion of sequences sgrna found in nodes and in leaves. Then return the
-    results
+    Definition
     """
-    if dic_nodes and dic_leaves:
-        # Only keep common keys between these 2 dictionnaries
-        keys_leaves = set(dic_leaves.keys())
-        keys_nodes = set(dic_nodes.keys())
-        common_keys = list(keys_leaves & keys_nodes)
-        dic_fusion = {}
-        # Fusion of dict for common sequences
-        for sgrna in common_keys:
-            dic_fusion[sgrna] = {}
-            dic_fusion[sgrna] = {dic_leaves[sgrna], dic_nodes[sgrna]}
-        return dic_fusion
-    # No leaves to add to the intersection
-    elif dic_nodes:
-        return dic_nodes
-    # No nodes to add to the intersection
-    else:
-        return dic_leaves
+    # The list of path is empty
+    if not list_nodes: return dic_ref
+
+    node_path = list_nodes[0]
+    dic_node = pickle.load(open(node_path, "rb"))
+    # Retrieve keys for the new node and old nodes
+    keys_node = set(dic_node.keys())
+    keys_ref = set(dic_ref.keys())
+    # Common keys between new and old nodes
+    common_keys = list(keys_ref & keys_node)
+    dic_inter_nodes = {}
+    # Fusion of dict for common sequences
+    for sgrna in common_keys:
+        dic_inter_nodes[sgrna] = {}
+        dic_inter_nodes[sgrna] = {dic_ref[sgrna], dic_node[sgrna]}
+    return fusion_nodes(dic_inter_nodes, list_nodes[1: ])
+
+
+def fusion_node_leaf(list_leaves, dic_node, fasta_path, dict_org_code, workdir,
+                     uncompressed_file_path):
+    """
+    Definition
+    """
+    nb_file = 4
+    cf.unzip_files(uncompressed_file_path, list_leaves, dict_org_code, workdir)
+    sorted_genomes = cf.sort_genomes(list_leaves, fasta_path, dict_org_code,
+                                     False)
+    sorted_genomes = [(genome, "in") for genome in sorted_genomes]
+    print(sorted_genomes)
+    list_fasta = cf.write_to_fasta_parallel(dic_node, nb_file, workdir)
+    dic_seq = ag.principal_search(sorted_genomes, list_fasta, dict_org_code,
+                                  dic_node, "NGG", 20, workdir, time.time())
+    cf.delete_used_files(workdir)
+    return dic_seq
+
+
+def write_node_file(list_leaves, list_nodes, dic_fusion, path_to_write):
+    """
+    Intersection dictionnary, containing the list of nodes and leaves necessary
+    to construct this node and the dictionnary containing sgRNA sequences and
+    coordinates of each organism being part of the node
+    """
+    dic_inter = {}
+    dic_inter["metadata"] = list_leaves + list_nodes
+    dic_inter["data"] = dic_fusion
+    name_node = "quatre"
+    if not os.path.isdir(path_to_write):
+        os.mkdir(path_to_write)
+    pickle.dump(dic_inter, open(path_to_write + name_node + ".p", "wb"),
+                                protocol=3)
 
 
 if __name__ == '__main__':
@@ -69,24 +106,22 @@ if __name__ == '__main__':
     FASTA_PATH = WORKDIR + "/reference_genomes/fasta"
     DICT_ORGANISM_CODE = cf.read_json_dic(PARAM.rfg +
                                           '/genome_ref_taxid.json')
-    dic_leaves = {}
-    dic_nodes = {}
 
-    if LIST_LEAVES:
-            dic_leaves = ag.construction(FASTA_PATH, "NGG", 20, LIST_LEAVES, [],
-                                         DICT_ORGANISM_CODE, WORKDIR, PARAM.rfg)
-    if LIST_NODES:
-        pass
-
-    dic_fusion = fusion_dic(dic_leaves, dic_nodes)
+    if LIST_LEAVES and not LIST_NODES:
+        cf.eprint("Intersection of leaves")
+        dic_fusion = ag.construction(FASTA_PATH, "NGG", 20, LIST_LEAVES, [],
+                                     DICT_ORGANISM_CODE, WORKDIR, PARAM.rfg)
+    else:
+        cf.eprint("Intersection of nodes")
+        DIC_NODE = fusion_nodes(pickle.load(open(LIST_NODES[0], "rb")),
+                                LIST_NODES[1: ])
+        if LIST_LEAVES:
+            cf.eprint("Intersection of nodes and leaves")
+            dic_fusion = fusion_node_leaf(LIST_LEAVES, DIC_NODE["data"], FASTA_PATH,
+                                          DICT_ORGANISM_CODE, WORKDIR, PARAM.rfg)
+        else:
+            dic_fusion = DIC_NODE
 
     if DEBUG: cf.eprint(str(len(dic_fusion)) + " hits remain after ")
 
-    # Grand dictionnaire contenant le dic_fusion et une clef pour les metadata
-    dic_inter = {}
-    dic_inter["metadata"] = LIST_LEAVES + LIST_NODES
-    dic_inter["data"] = dic_fusion
-    name_node = "trois"
-    if not os.path.isdir(PARAM.rfg + "/node"):
-        os.mkdir(PARAM.rfg + "/node")
-    pickle.dump(dic_inter, open(PARAM.rfg + "/node/" + name_node + ".p", "wb"), protocol=3)
+    write_node_file(LIST_LEAVES, LIST_NODES, dic_fusion, PARAM.rfg + "/node/")
