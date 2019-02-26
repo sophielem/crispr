3f273b17a77ca69158d9042fda44c3a014771587
sophielem <lematre.sophiegmail.com>
2019-02-26 14:35:44 +0100

    cut in func

--- scan_tree.py
+++ scan_tree.py
@@ -9,7 +9,7 @@ import json
 import argparse
 import pickle
 from ete3 import NCBITaxa
-# import intersection as inter
+import intersection as inter
 
 DEBUG = True
 
@@ -45,12 +45,13 @@ def setup_application(parameters, dict_organism_code):
     return list_taxid_in, list_taxid_notin
 
 
-def create_topo_tree(dict_org, param):
+def create_topo_tree(dict_org):
     """
     Create a topology tree with the organism dictionary given in parameter
     """
     # List of taxon ID of all genomes
-    all_spc = [DICT_ORG[sp][1] for sp in param.gi.split('&')] if DEBUG else [DICT_ORG[sp][1] for sp in DICT_ORG]
+    all_spc = [dict_org[sp][1] for sp in dict_org]
+    if DEBUG: all_spc = [dict_org[sp][1] for sp in PARAM.gi.split('&')]
     # Create the entire topology tree
     ncbi = NCBITaxa()
     return ncbi.get_topology(all_spc)
@@ -80,39 +81,64 @@ def traversing_tree(subtree, list_taxid):
 
 def issubnode(node_ref, nodes_name):
     """
-    Definition
+    Check if a given node is a child or a parent of a list of nodes.
+    If it is a child, the given node is not appended to the list, else it is
+    appended to the list. Moreover, if it is a parent to a node in the list,
+    this node is removed
     """
-    # Pas le nom, il faut l'objet noeud !!!!!!!
+    if node_ref.name in nodes_name:
+        return nodes_name
+    # Retrieve names of all children of the node given
     children_name = [child.name for child in node_ref.children if not child.is_leaf()]
-    # Enlever le noeud enfant de la liste et ajouter ce noeud
+    # Remove all children from the list
     to_remove = [child for child in children_name if child in nodes_name]
     nodes_name = [name for name in nodes_name if name not in to_remove]
-
+    # Check if a parent exists, otherwise it is a root and it is appended to
+    # the list
     if not hasattr(node_ref.up, "name"):
         nodes_name.append(node_ref.name)
-    elif node_ref.up.name not in nodes_name and node_ref.name not in nodes_name: # Ne pas ajouter le noeud dans la liste
+    # Check if the parent of the node is not in the list, and then append it
+    elif node_ref.up.name not in nodes_name:
         nodes_name.append(node_ref.name)
     return nodes_name
+
+
+def inter_node_complete(list_taxid, tree_topo, rfg):
+    """
+    Definition
+    """
+    nodes_checked = []
+    for leaf_id in list_taxid:
+        if traversing_tree(tree_topo.get_leaves_by_name(leaf_id)[0].up, list_taxid):
+            # Check this node is not a subnode of a node in the list
+            nodes_checked = issubnode(tree_topo.get_leaves_by_name(leaf_id)[0].up, nodes_checked)
+    path_nodes_checked = [rfg + "/node/inter/" + node + ".p" for node in nodes_checked]
+    # Return the intersection of checked nodes
+    if len(path_nodes_checked) > 1:
+        return inter.inter_nodes(pickle.load(open(path_nodes_checked[0], "rb"))["data"], path_nodes_checked[1:])
+    elif len(path_nodes_checked) == 1:
+        return pickle.load(open(path_nodes_checked[0], "rb"))["data"]
+    else:
+        return {}
+
+
+def find_node_complete(dict_org, list_taxid_in, list_taxid_notin, rfg):
+    """
+    Definition
+    """
+    tree_topo = create_topo_tree(dict_org)
+
+    dic_node_in = inter_node_complete(list_taxid_in, tree_topo, rfg)
+    dic_node_notin = inter_node_complete(list_taxid_notin, tree_topo, rfg)
+    print(len(dic_node_in))
+    return dic_node_in, dic_node_notin
 
 
 if __name__ == '__main__':
     PARAM = args_gestion()
     DICT_ORG = json.load(open(PARAM.rfg + '/genome_ref_taxid.json','r'))
     list_taxid_in, list_taxid_notin = setup_application(PARAM, DICT_ORG)
-    tree_topo = create_topo_tree(DICT_ORG, PARAM)
+    find_node_complete(DICT_ORG, list_taxid_in, list_taxid_notin, PARAM.rfg)
 
-    nodes_in = []
-    nodes_notin = []
-    for leaf_id in list_taxid_in:
-        if traversing_tree(tree_topo.get_leaves_by_name(leaf_id)[0].up, list_taxid_in):
-            if DEBUG: print(tree_topo.get_leaves_by_name(leaf_id)[0].up.name)
-            nodes_in = issubnode(tree_topo.get_leaves_by_name(leaf_id)[0].up, nodes_in)
-            
-            # dic_node_in = inter.inter_ndoes(pickle.load(open(nodes_in_name[0], "rb"))["data"], nodes_in_name[1:])
-    for leaf_id in list_taxid_notin:
-        if traversing_tree(tree_topo.get_leaves_by_name(leaf_id)[0].up, list_taxid_notin):
-            if DEBUG: print(tree_topo.get_leaves_by_name(leaf_id)[0].up.name)
-            nodes_notin.append(tree_topo.get_leaves_by_name(leaf_id)[0].up.name)
-            dic_node_notin = inter.inter_ndoes(pickle.load(open(nodes_notin[0], "rb"))["data"], nodes_notin[1:])
 # Exemple commande
 # ./scan_tree.py -rfg ../reference_genomes_pickle -gi "Streptosporangium roseum DSM 43021 GCF_000024865.1&Catenulispora acidiphila DSM 44928 GCF_000024025.1&Streptomyces violaceusniger Tu 4113 GCF_000147815.2&Streptomyces bingchenggensis BCW-1 GCF_000092385.1&Archangium gephyra GCF_001027285.1" -gni ""
