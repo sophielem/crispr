f390a305a4466e88007a1fbc6322fa0ee18ad507
sophielem <lematre.sophiegmail.com>
2019-03-18 16:54:00 +0100

    add / to database path

--- crispr_workflow.sh
+++ crispr_workflow.sh
@@ -22,8 +22,8 @@ if [ "$URL_CRISPR" = "" ]; then
     error_json
 fi
 
-echo "setCompare -i \"$gi\" -o \"$gni\" -l $rfg/index -e index" > ./cmd.txt
-setCompare -i "$gi" -o "$gni" -l $rfg/index -e index 2>> ./setCompare.err 1> ./setCompare.log
+echo "setCompare -i \"$gi\" -o \"$gni\" -l $rfg/index/ -e index" > ./cmd.txt
+setCompare -i "$gi" -o "$gni" -l $rfg/index/ -e index 2>> ./setCompare.err 1> ./setCompare.log
 echo "post_processing.py -sl 20 -pam \"NGG\" -gi \"$gi\" -gni \"$gni\" -r \"$URL_CRISPR\"  -c 2000" >> ./cmd.txt
 python $CRISPR_TOOL_SCRIPT_PATH/post_processing.py -sl 20 -pam "NGG" -gi "$gi" -gni "$gni" -r "$URL_CRISPR" -c 2000 2>> ./post_processing.err 1> ./post_processing.log
 
