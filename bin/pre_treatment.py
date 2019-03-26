05e09a9517d6a2ca997812a0f5b1077c28809fcd
sophielem <lematre.sophiegmail.com>
2019-03-26 16:57:00 +0100

    module word_detect to create pickle

--- bin/pre_treatment.py
+++ bin/pre_treatment.py
@@ -7,13 +7,13 @@ import argparse
 import json
 import sys
 import shutil
-import pickle
 import re
 from Bio import SeqIO
 from Bio import Entrez
 from ete3 import NCBITaxa
 from tqdm import tqdm
 import couchBuild
+import word_detect
 import wordIntegerIndexing as decoding
 import pycouch.wrapper as couchDB
 
@@ -263,88 +263,6 @@ def check_genome_exists(filename, gcf, asm, taxid, rfg):
 
 
 # FIND ALL SGRNA
-def complement_seq(sequence):
-    """
-    Function for turning a 5'-3' nucleotidic sequence into
-    its 5'-3' reverse complement.
-    """
-    rev_comp = []
-    complement = {"A" : "T", "C" : "G", "T" : "A", "G" : "C"}
-    rev_comp = [complement[sequence[idx]] if sequence[idx] in complement.keys() else "N" for idx in range(len(sequence) - 1, -1, -1)]
-    return "".join(rev_comp)
-
-
-def build_expression(seq):
-    """
-    Build the regular expression by replacing the letter by iupac code or
-    the letter itself
-    """
-    result = ''
-    iupac_code = {'R': '[AG]', 'Y': '[CT]', 'S': '[GC]', 'W': '[AT]',
-                  'K': '[GT]', 'M': '[AC]', 'B': '[CGT]', 'D': '[AGT]',
-                  'H': '[ACT]', 'V': '[ACG]', 'N': '[ACGT]'}
-    for letter in seq:
-        if letter in iupac_code:
-            result = result + iupac_code[letter]
-        else:
-            result = result + letter
-    return result
-
-
-def find_indices_sgrna(seq, pam):
-    """
-    Uses Regular expression matching of the pam motif to the reference genome
-    to get the start positions (0-based) of each match
-    """
-    reg_exp = build_expression(pam)
-    indices = [m.start() for m in re.finditer('(?=' + reg_exp + ')', seq, re.I)]
-    return indices
-
-
-def find_sgrna_seq(seq_list, len_seq, reverse, str_reverse, seq_dict, genome_seq, organism, ref):
-    """
-    Uses start index and the length of the sequence to get the sequence and
-    fill the dictionary containing all sgRNA sequences with coordinates
-    """
-    for indice in seq_list:
-        end = indice + len_seq
-        seq = genome_seq[indice:end] if reverse else genome_seq[indice:end].reverse_complement()
-        seq = str(seq)
-        if seq not in seq_dict:
-            seq_dict[seq] = {organism: {}}
-        if ref not in seq_dict[seq][organism]:
-            seq_dict[seq][organism][ref] = []
-        seq_dict[seq][organism][ref].append(str_reverse + str(indice+1) + ',' +
-                                            str(end) + ')')
-    return seq_dict
-
-
-def construct_in(organism, organism_code, rfg, pam="NGG", non_pam_motif_length=20):
-    """
-    Construct the sequences for first organism,
-    with python regular expression research
-    """
-    print("SEARCH SGRNA")
-    fasta_file = (rfg + '/genome_fasta/' + organism_code + '_genomic.fna')
-    sgrna = "N" * non_pam_motif_length + pam
-
-    seq_dict = {}
-
-    for genome_seqrecord in SeqIO.parse(fasta_file, "fasta"):
-        genome_seq = genome_seqrecord.seq
-        ref = genome_seqrecord.id
-        seq_list_forward = find_indices_sgrna(str(genome_seq),
-                                              complement_seq(sgrna))
-        seq_list_reverse = find_indices_sgrna(str(genome_seq), sgrna)
-
-        seq_dict = find_sgrna_seq(seq_list_forward, len(pam) + non_pam_motif_length,
-                                  False, "+(", seq_dict, genome_seq, organism, ref)
-        seq_dict = find_sgrna_seq(seq_list_reverse, len(pam) + non_pam_motif_length,
-                                  True, "-(", seq_dict, genome_seq, organism, ref)
-
-    pickle_file = (rfg + "/genome_pickle/" + organism + ".p")
-    pickle.dump(seq_dict, open(pickle_file, "wb"), protocol=3)
-    return pickle_file
 
 
 def indexation(name_file, rfg, pickle_file):
@@ -426,7 +344,8 @@ if __name__ == '__main__':
         # Create dictionnary with all taxon ID
         REF_NEW = check_genome_exists(PARAM.file, GCF, ASM,
                                       TAXID, PARAM.rfg)
-        PICKLE_FILE = construct_in(NAME, REF_NEW, PARAM.rfg)
+        PICKLE_FILE = word_detect.construct_in(PARAM.rfg + "/genome_pickle/" + NAME,
+                                               PARAM.rfg + '/genome_fasta/' + REF_NEW + '_genomic.fna')
         indexation(NAME, PARAM.rfg, PICKLE_FILE)
         print("SUCCESS&Created metafile for {}".format(NAME))
 
