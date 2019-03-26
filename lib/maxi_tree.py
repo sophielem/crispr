6920dfdfb80143de12158c96960a84575dd278c7
sophielem <lematre.sophiegmail.com>
2019-03-26 17:05:50 +0100

    WiP: module maxi tree

---
+++ lib/maxi_tree.py
@@ -0,0 +1,22 @@
+class MaxiTree(object):
+    """docstring for MaxiTree."""
+    def __init__(self, arg):
+        super(MaxiTree, self).__init__()
+        self.arg = arg
+
+    def is_member(self, taxid):
+        pass
+
+    def insert(self, taxid):
+        pass
+
+    def dump(self, arg):
+        """
+        Va renvoyer un arbre suivant la méthode demandée : en entier, seulement les taxon, seulement les noms communs
+        """
+        pass
+
+
+
+# SEUL LE CONSTRUCTEUR SERA DIFFERENT SUIVANT LES ARGUMENTS DONNÉS EN ENTREE
+# CELA PEUT ETRE 2 FICHIERS, COMME LE CAS ACTUEL OU UN SEUL COMME L ARBRE ENTIER CONTENANT LES TAXON ET LES NOMS COMMUNS DES ORGANISMES
