29ba6568b38466976fc36814d0c5ce59a56ec959
sophielem <lematre.sophiegmail.com>
2019-03-29 16:14:23 +0100

    docstrings, comments and replace list_occ by ref in CoordSeq

--- bin/specific_gene.py
+++ bin/specific_gene.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
+"""
+Definition
+"""
 
+import sys
 import argparse
 import pickle
+from collections import OrderedDict
 import wordIntegerIndexing as decoding
 import display_result as dspl
 import post_processing as pp
@@ -10,7 +15,7 @@ from parse_blast import BlastReport, BlastHit
 
 class ResumeSeq(object):
     """docstring for ResumeSeq."""
-    def __init__(self, dic_org):
+    def __init__(self, dic_org=None):
         self.proportion = 0
         self.dic_org = dic_org
 
@@ -18,41 +23,67 @@ class ResumeSeq(object):
         return "Proportion : {}\tTotal occurences : {}".format(self.proportion, self.total_occ())
 
     def cal_proportion(self, nb_total):
+        """
+        Calcul proportion of organism present in the dictionary
+        """
         self.proportion = len(self.dic_org) / int(nb_total)
         return self.proportion
 
-    def total_occ(self, org_name):
-        return self.dic_org[org].total_occ()
-
-    def total_occ(self):
+    def total_occ(self, org_name=None):
+        """
+        Calcul the total number of occurences for the sequence or for a given organism
+        for a sequence
+        """
         occ = 0
-        for org_name in self.dic_org:
-            occ += self.total_occ(org_name)
+        if org_name:
+            for coord_seq in self.dic_org[org_name]:
+                occ += coord_seq.total_occ()
+        else:
+            for org_name in self.dic_org:
+                occ += self.total_occ(org_name)
         return occ
 
     def update(self, dict_org):
+        """
+        Update the dictionary
+        """
         if dict_org:
             self.dic_org.update(dict_org)
 
     def org_names(self):
+        """
+        Return a list of organism name which have this sequence
+        """
         return list(self.dic_org.keys())
 
     def write(self, genomes_in):
+        """
+        Return a string of coordinates for each reference for each organism
+        given an order by the list
+        """
         to_write = ""
-        for gi in genomes_in:
-            if gi in self.org_names():
-                for ref in self.dic_org[gi]:
-                    to_write += ref + ':' + ','.join(self.dic_org[gi][ref]) + ';'
+        for gen_in in genomes_in:
+            if gen_in in self.org_names():
+                for coord_seq in self[gen_in]:
+                    to_write += coord_seq.ref + ':' + ','.join(coord_seq.list_coord) + ';'
             else:
                 to_write += "None ;"
         to_write += self.total_occ()
         return to_write.strip(";")
 
     def list_ref(self, org_name):
-        list_ref = [{"ref": ref, "coords": self.dic_org[org_name][ref].list_coord} for ref in self.dic_org[org_name]]
+        """
+        Return a list of dictionaries containing coordinates of the sequence for
+        a reference organism
+        """
+        list_ref = [{"ref": coord_seq.ref, "coords": coord_seq.list_coord} for coord_seq in self.dic_org[org_name]]
         return list_ref
 
     def list_occ(self):
+        """
+        Return a list of dicionaries containing a list_ref for all references for
+        each organism
+        """
         list_occurences = [{'org': genome, 'all_ref': self.list_ref(genome)} for genome in self.org_names()]
         return list_occurences
 
@@ -60,28 +91,29 @@ class ResumeSeq(object):
 
 class CoordSeq(object):
     """docstring for CoordSeq."""
-    def __init__(self):
-        self.list_coord = []
-        self.list_occ = []
-
-    def __init__(self, coords, occs):
+    def __init__(self, coords=None, ref=None):
         self.list_coord = coords
-        self.list_occ = occs
+        self.ref = ref
 
     def __repr__(self):
         return "Total occurences : {}\tFor {} coordinates".format(self.total_occ(), len(self))
 
-    def update(self, coord, occ):
+    def __len__(self):
+        return len(self.list_coord)
+
+    def update(self, coord):
+        """
+        Update the coordinate list
+        """
         if coord not in self.list_coord:
             self.list_coord.append(coord)
-            self.list_occ.append(occ)
             return True
         return False
 
-    def total_occ():
-        return sum(self.list_occ)
-
-    def __len__(self):
+    def total_occ(self):
+        """
+        The number of coordinates for the reference
+        """
         return len(self.list_coord)
 
 
@@ -110,37 +142,37 @@ def args_gestion():
                         help="The top hits to download",
                         default=1000)
     args = parser.parse_args()
-
     return args
 
 
 def on_gene(sgrna, gene):
+    """
+    Check if a given sgrna is on a given gene
+    """
     return gene.start <= sgrna[0] and gene.end >= sgrna[1]
 
 
-def coord_on_gene(list_coord, list_gene):
+def coord_on_gene(list_coord, list_gene, ref):
     """
-    Definition
+    For a reference organism check if all sgrna are on genes
+    Return a CoordSeq object if at least one sgrna is on a gene
     """
-    coord_seq = CoordSeq()
+    coord_seq = CoordSeq(ref=ref)
     for coord in list_coord:
-        nb_on_gene = len(list(filter(lambda x: on_gene(coord, x), list_gene)))
-        if nb_on_gene != 0:
-            coord_seq.update(coord, nb_on_gene)
-    if len(coord_seq) != 0:
+        nb_on_gene = list(filter(lambda x: on_gene(coord, x), list_gene))
+        if nb_on_gene:
+            coord_seq.update(coord)
+    if coord_seq.list_coord:
         return coord_seq
     return None
 
 
 def check_on_gene(blast_file, dic_index, nb_gi):
     """
-    La sortie de blast est plus courte : peut être qu'un seul organisme aura un
-    gène homologue.
-    1. Boucler sur les séquences
-    2. Ne garder que les organismes présents dans blast
-    3. Vérifier que sgrna est sur un gène tout en checkant la référence
+    Return a dictionary of ResumeSeq object for each sequence which is at least
+    on one gene in one organism.
     """
-    blastoutput = pikle.load(open(blast_file, "rb"))
+    blastoutput = pickle.load(open(blast_file, "rb"))
     resume_seq = {}
     # For each sgrna
     for seq in dic_index:
@@ -151,21 +183,22 @@ def check_on_gene(blast_file, dic_index, nb_gi):
             tmp = {}
             for ref in blastoutput.ref_names(org):
                 # Find the list of coordinates on gene
-                coord_seq = coord_on_gene(dic_index[seq].genomes_dict[org][ref], blastoutput.gene_coords(org, ref))
+                coord_seq = coord_on_gene(dic_index[seq].genomes_dict[org][ref],
+                                          blastoutput.gene_coords(org, ref), ref)
                 if coord_seq:
-                    if not tmp:
-                        tmp[org] = {}
-                    tmp[org][ref] = coord_seq
+                    tmp[org].append(coord_seq)
             resume_seq[seq].update(tmp)
-        if len(resume_seq[seq].dic_org) == 0:
+        # If no coordinate in the ResumeSeq object, then delete the sgrna from the dictionary
+        if not resume_seq[seq].dic_org:
             del resume_seq[seq]
         else:
             resume_seq[seq].cal_proportion(nb_gi)
+    # If resume_seq is not empty, then at least one sgrna is on one gene
     if resume_seq:
         return resume_seq
-    else:
-        print("Progam terminated&No hits on genes")
-        sys.exit()
+    # None sgrna on homologous gene
+    print("Progam terminated&No hits on genes")
+    sys.exit()
 
 
 if __name__ == "__main__":
@@ -175,14 +208,16 @@ if __name__ == "__main__":
     DIC_INDEX, NB_HITS = pp.parse_setcompare_out(PARAM.f, None)
     if DIC_INDEX:
         LEN_SEQ = int(PARAM.sl) + int(len(PARAM.pam))
+        DIC_HITS = OrderedDict()
         for rank in DIC_INDEX:
-            sequence = decoding.decode(rank, ["A", "T", "C", "G"], len_seq)
+            sequence = decoding.decode(rank, ["A", "T", "C", "G"], LEN_SEQ)
             DIC_HITS[sequence] = dspl.Hit(sequence, DIC_INDEX[rank])
-            # Search coordinates for each sgrna in each organism
+        # Search coordinates for each sgrna in each organism
         DIC_HITS = pp.couchdb_search(DIC_HITS, GENOMES_IN, PARAM.r, int(PARAM.c), PARAM.no_proxy)
-
+        # Keep sgrna which are on gene
         RESUME_SEQ = check_on_gene(PARAM.blast, DIC_HITS, len(GENOMES_IN))
-        RESUME_SEQ = sorted(RESUME_SEQ, key=lambda hit:hit.proportion, reverse=True)
+        # Sort sgrna by the proportion of organism containing this sgrna on a gene
+        RESUME_SEQ = sorted(RESUME_SEQ, key=lambda hit: hit.proportion, reverse=True)
         dspl.display_hits(RESUME_SEQ, GENOMES_IN, GENOMES_NOTIN,
                           PARAM.pam, int(PARAM.sl), ".", int(PARAM.nb_top))
         print(','.join(GENOMES_NOTIN))
