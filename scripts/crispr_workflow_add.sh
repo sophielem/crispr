40136b8d867f9f05f693656bfdeb2e435e1eb2f5
sophielem <lematre.sophiegmail.com>
2019-04-15 17:08:23 +0200

    update worklow if user gives taxonomy info

--- scripts/crispr_workflow_add.sh
+++ scripts/crispr_workflow_add.sh
@@ -5,20 +5,32 @@ error_json () {
     cat fail.log
 }
 
-if [ "$pam" != "NGG" ]; then
+if [ "$URL_TAXON" != "" ]; then
     error_json
 elif [ "$CRISPR_TOOL_SCRIPT_PATH" = "" ]; then
     error_json
 
 elif  [ "$URL_CRISPR" = "" ]; then
     error_json
+elif [ "$DB_NAME" = "" ]; then
+    error_json
 else
 
-    url_db=""
-    if [ $COMMAND = "all" ];then
-        url_db="-url $URL_CRISPR -tree $TREE -m $MAP_BD"
-    fi
-    echo python $CRISPR_TOOL_SCRIPT_PATH/pre-treatment.py $COMMAND -file $FASTA_FILE $url_db 2> ./pre_treatment.err 1> ./pre_treatment.log
+    # url_db=""
+    # if [ $COMMAND = "all" ];then
+    #     url_db="-url $URL_CRISPR -tree $TREE -m $MAP_BD"
+    # fi
+    # echo python $CRISPR_TOOL_SCRIPT_PATH/pre-treatment.py $COMMAND -file $FASTA_FILE $url_db 2> ./pre_treatment.err 1> ./pre_treatment.log
+
+    echo python check_taxonomy -taxid $TAXID -gcf $GCF -out $INFO_FILE -url $URL_TAXON -dbName $DB_NAME
+
+    NAME_FILE=$(echo $ORG_NAME $GCF)
+
+    echo python create_metafile.py -file $FASTA_FILE -out $NAME_FILE
+
+    if [ $SINGLE = "True"]; then
+        echo python couchBuild --url $URL_CRISPR --map $MAP_FILE --data NAME_FILE".p"
+        echo python couchBuild $DB_NAME --url $URL_TAXON --data $INFO_FILE
 
     if grep "Program terminated" ./pre_treatment.log > /dev/null;
     then
