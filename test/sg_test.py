b6baf671a466f113e1f5882a7c9b6cc95a8436a0
sophielem <lematre.sophiegmail.com>
2019-04-16 15:23:42 +0200

    add quote for gi argument

--- test/sg_test.py
+++ test/sg_test.py
@@ -9,7 +9,7 @@ import difflib
 if __name__ == '__main__':
     gi = "Enterobacter sp. 638 GCF_000016325.1&Candidatus Blochmannia vafer str. BVAF GCF_000185985.2"
     os.system("python -u bin/parse_blast.py -blast test/data/sg/blast.xml -gi \"" + gi + "\" -o test/parse_blast.p -ip 70")
-    os.system("python -u bin/specific_gene.py -f test/data/sg/output_c.txt -sl 20 -pam \"NGG\" -gi " + gi + " -gni \"\" -r " + sys.argv[1] + "  -c 2000 --no-proxy -blast test/parse_blast.p")
+    os.system("python -u bin/specific_gene.py -f test/data/sg/output_c.txt -sl 20 -pam \"NGG\" -gi \"" + gi + "\" -gni \"\" -r " + sys.argv[1] + "  -c 2000 --no-proxy -blast test/parse_blast.p")
 
     res = json.load(open("test/results.json", "r"))
 
