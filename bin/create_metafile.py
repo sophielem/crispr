b14c20142a5c8cfad47eb5072c4ab3b514303d3f
sophielem <lematre.sophiegmail.com>
2019-04-17 10:19:33 +0200

    add rfg arg

--- bin/create_metafile.py
+++ bin/create_metafile.py
@@ -16,14 +16,19 @@ def args_gestion():
     parser.add_argument("-out", metavar="<str>",
                         help="The output path without the extension",
                         required=True)
+    parser.add_argument("-rfg", metavar="<str>",
+                        help="The path to the database for index and pickle file",
+                        nargs="?", const="")
     args = parser.parse_args()
     return args
 
 
 if __name__ == '__main__':
     PARAM = args_gestion()
-    DIC_PICKLE = word_detect.construct_in(PARAM.file, PARAM.out + ".p")
+    PATH = PARAM.rfg + "/genome_pickle/" if PARAM.rfg else ""
+    DIC_PICKLE = word_detect.construct_in(PARAM.file, PATH + PARAM.out + ".p")
     if DIC_PICKLE:
-        decoding.indexAndOccurencePickle(PARAM.out + ".p", PARAM.out + ".index")
+        PATH = PARAM.rfg + "/genome_index/" if PARAM.rfg else ""
+        decoding.indexAndOccurencePickle(PARAM.out + ".p", PATH + PARAM.out + ".index")
     else:
         print("Program terminated&No sgRNA sequences on the query")
