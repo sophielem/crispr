2a2c405c2b97a36f70dbe36358ca71e082f8a1de
Sophie Lematre <lematre.sophie@gmail.com>
2019-07-11 10:00:00 +0200

    Update display_result.py

--- lib/display_result.py
+++ lib/display_result.py
@@ -114,7 +114,8 @@ def display_hits(dic_hits, genomes_in, genomes_not_in, pam, non_pam_motif_length
         for seq in dic_hits:
             for genome in genomes_in:
                 for coord_seq in dic_hits[seq].dic_org[genome]:
-                    if coord_seq.ref not in dic_sorted_by_org[genome]:
+                    ref = coord_seq.ref
+                    if ref not in dic_sorted_by_org[genome]:
                         dic_sorted_by_org[genome][ref] = {}
                     dic_sorted_by_org[genome][ref][seq] = coord_seq.list_coord
     json.dump(dic_sorted_by_org, open("results_by_org.json", "w"))
